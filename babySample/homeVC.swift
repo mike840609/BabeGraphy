eaa61db850caca27c466ca7b75050f5bca355dcc
mike840609 <mike840609@gmail.com>
2016-07-01 18:39:01 +0800

    homeVC loadData

--- babySample/homeVC.swift
+++ babySample/homeVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 
 class homeVC: UICollectionViewController {
@@ -24,20 +26,70 @@ class homeVC: UICollectionViewController {
         // background color
         collectionView?.backgroundColor = .whiteColor()
         
-        //
+        // logoutBtn Set
         let attributes = [NSFontAttributeName: UIFont.fontAwesomeOfSize(20)] as Dictionary!
         logoutBtn.setTitleTextAttributes(attributes, forState: .Normal)
         logoutBtn.title = String.fontAwesomeIconWithName(.SignOut)
+        
+        
+        
     }
     
     
     
     // MARK: UICollectionViewDataSource
     
-    // header
+    // header View
     override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
         
         let header = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: "Header", forIndexPath: indexPath) as! headerView
+        
+        // 抓不到token ,token過期 ,重新登入一次
+        guard let AccessToken:String? = NSUserDefaults.standardUserDefaults().stringForKey("AccessToken") else {
+            
+            NSUserDefaults.standardUserDefaults().removeObjectForKey(ACCESS_TOKEN)
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            // 轉跳登入畫面
+            let signin = self.storyboard?.instantiateViewControllerWithIdentifier("LoginController") as! LoginController
+            let appDelegate : AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            appDelegate.window?.rootViewController = signin
+            
+        }
+        
+        //        print(AccessToken)
+        
+            Alamofire.request(.GET, "http://140.136.155.143/api/user/token/\(AccessToken!)").validate().responseJSON{ (response) in
+                
+                switch response.result{
+                case .Success(let json):
+                    let json = SwiftyJSON.JSON(json)
+                    
+                    
+                    // optional chainging
+                    guard let id:String = json["data"][0]["id"].stringValue,
+                        let name:String = json["data"][0]["name"].stringValue,
+                        let email:String = json["data"][0]["email"].stringValue,
+                        let follower:String = json["data"][0]["follower_count"].stringValue,
+                        let following:String = json["data"][0]["followed_count"].stringValue
+                        else {return}
+                    
+                    
+                    
+                    header.fullnameLbl.text = name.uppercaseString
+                    header.posts.text  = "0"
+                    header.followers.text = follower
+                    header.followings.text = following
+                    
+                    print("id:\(id)\n name:\(name)\n email:\(email)\n follower:\(follower)\n following:\(following)\n==========================================")
+                    
+                    
+                case .Failure(let error):
+                    print(error.localizedDescription)
+                    
+                }
+        }
+        
         
         return header
     }
@@ -58,6 +110,15 @@ class homeVC: UICollectionViewController {
         return cell
     }
     
+    // MARK: - Customer Function
+    func getInfo(){
+        
+        
+        
+        
+    }
+    
+    // MARK: - IBAction
     @IBAction func logout(sender: AnyObject) {
         
         NSUserDefaults.standardUserDefaults().removeObjectForKey(ACCESS_TOKEN)
